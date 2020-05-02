f932521b99ec54506d12dc0f6e3ba555c2c53e05
m-dary-nur <m.dary.nur@gmail.com>
2020-05-02 15:22:35 +0700

    Updates

---
+++ precache-manifest.67e918b59de22263a3ad85358cf39fbd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e23f0b56bec28eaf9ceb7991f515e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "589d9872dfead82fd76a",
+    "url": "/static/css/main.824edb9b.chunk.css"
+  },
+  {
+    "revision": "2340d620b6b8f1549376",
+    "url": "/static/js/0.f575eeac.chunk.js"
+  },
+  {
+    "revision": "157441b97caf389390c1",
+    "url": "/static/js/1.88471bdb.chunk.js"
+  },
+  {
+    "revision": "6e9f1de1799c64126526",
+    "url": "/static/js/2.58e6dfd3.chunk.js"
+  },
+  {
+    "revision": "32d7fc64bfb32e799f24",
+    "url": "/static/js/3.08c314a2.chunk.js"
+  },
+  {
+    "revision": "2c17a805dc6835b8cc34",
+    "url": "/static/js/4.b9bed5e1.chunk.js"
+  },
+  {
+    "revision": "b0eae34d4e1763a7e0cd",
+    "url": "/static/js/5.c0c15502.chunk.js"
+  },
+  {
+    "revision": "aeca6f08301c3b28b386",
+    "url": "/static/js/8.0ea76326.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/8.0ea76326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589d9872dfead82fd76a",
+    "url": "/static/js/main.c079bba9.chunk.js"
+  },
+  {
+    "revision": "5946728fce3615acfaac",
+    "url": "/static/js/runtime-main.c415f01d.js"
+  },
+  {
+    "revision": "ae11982814c26d1f2d5b6eac7d474a2f",
+    "url": "/static/media/cashier.ae119828.png"
+  },
+  {
+    "revision": "05cd8c1dd729bf663301ef592560c76d",
+    "url": "/static/media/clinic.05cd8c1d.png"
+  },
+  {
+    "revision": "19d9b58b365b07eb3aa91102a2acc092",
+    "url": "/static/media/consultant.19d9b58b.png"
+  },
+  {
+    "revision": "ffc5c2c38f5c5619443ed09293c5bad9",
+    "url": "/static/media/erp.ffc5c2c3.png"
+  },
+  {
+    "revision": "c4b290313b08b7412dad9154513c0d11",
+    "url": "/static/media/inventory.c4b29031.png"
+  },
+  {
+    "revision": "12228cec47dec572d15660415d3179ef",
+    "url": "/static/media/museum.12228cec.png"
+  },
+  {
+    "revision": "8150a93825a320f9cbf51ad9f73952b0",
+    "url": "/static/media/pos.8150a938.png"
+  },
+  {
+    "revision": "c35f3d7b223665e1b96d1f3796fd5f58",
+    "url": "/static/media/profile.c35f3d7b.png"
+  }
+]);
